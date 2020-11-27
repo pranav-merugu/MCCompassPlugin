d2db15e8c51005103a02b8172537435829a3ec26
Albert Chen <chenalbert06@gmail.com>
2020-11-27 14:10:27 -0500

    powered creepers

--- MCCompassPlugin/src/main/java/dev/cibmc/spigot/blankplugin/App.java
+++ MCCompassPlugin/src/main/java/dev/cibmc/spigot/blankplugin/App.java
@@ -1,14 +1,30 @@
 package dev.cibmc.spigot.blankplugin;
+
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class App extends JavaPlugin {
+public class App extends JavaPlugin implements Listener {
+
     @Override
     public void onEnable() {
-        getLogger().info("Hello, SpigotMC!");
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
-    @Override
-    public void onDisable() {
-        getLogger().info("See you again, SpigotMC!");
-    }
+
+    @EventHandler
+    public void creaturespawn(CreatureSpawnEvent event) {
+        
+        if (event.getEntityType() == EntityType.CREEPER) {
+
+            Creeper creeper = (Creeper) event.getEntity();
+            creeper.setPowered(true);
+            
+        }
+    } 
 }
