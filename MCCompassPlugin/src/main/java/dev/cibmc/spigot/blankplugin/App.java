9e2cb7b4cb5a212742826331f6b4eebb11112442
DerpTaterTot <chenalbert06@gmail.com>
2020-11-28 13:22:01 -0500

    lodestone compass, not sure if functional

--- MCCompassPlugin/src/main/java/dev/cibmc/spigot/blankplugin/App.java
+++ MCCompassPlugin/src/main/java/dev/cibmc/spigot/blankplugin/App.java
@@ -2,6 +2,7 @@ package dev.cibmc.spigot.blankplugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CompassMeta;
@@ -29,31 +31,38 @@ public class App extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
         if (alias.equalsIgnoreCase("runner")) {
-            if (args.length == 0) {
-                sender.sendMessage("Please specify what you want do do with the runner!");
-            }
-            else {
-                if (args[0].equalsIgnoreCase("add")){
-                    if (runner != null) {
-                        sender.sendMessage("a runner has already been selected! try using /runner remove instead");
-                    }
-                    else if (Bukkit.getPlayerExact(args[1]) == null) {
-                        sender.sendMessage("This player does not exist");
-                    }
-                    else {
-                        Player target = Bukkit.getPlayerExact(args[1]);
-                        runner = target;
-                        sender.sendMessage("They have become a runner");
-                    }
-                }
-                else if (args[0].equalsIgnoreCase("remove")) {
-                    runner = null;
-                    sender.sendMessage("They have no longer become a runner");
+            Player player = (Player) sender;
+            if (player.isOp()) {
+                if (args.length == 0) {
+                    sender.sendMessage("Please specify what you want do do with the runner!");
                 }
                 else {
-                    sender.sendMessage("i don't understand what you mean");
+                    if (args[0].equalsIgnoreCase("add")){
+                        if (runner != null) {
+                            sender.sendMessage("a runner has already been selected! try using /runner remove instead");
+                        }
+                        else if (Bukkit.getPlayerExact(args[1]) == null) {
+                            sender.sendMessage("This player does not exist");
+                        }
+                        else {
+                            Player target = Bukkit.getPlayerExact(args[1]);
+                            runner = target;
+                            sender.sendMessage("They have become a runner");
+                        }
+                    }
+                    else if (args[0].equalsIgnoreCase("remove")) {
+                        runner = null;
+                        sender.sendMessage("They have no longer become a runner");
+                    }
+                    else {
+                        sender.sendMessage("i don't understand what you mean (try adding add or remove)");
+                    }
                 }
             }
+            else {
+                sender.sendMessage("your not op nice try :smooch: :mua:");
+            }
+
         }
 
         if (alias.equalsIgnoreCase("compass")) {
@@ -74,19 +83,20 @@ public class App extends JavaPlugin implements Listener {
         event.getPlayer().sendMessage("runner = " + runner);
     }   
 
-/*
+
     @EventHandler
     public void viewplayer(PlayerInteractEntityEvent event) {
         if (event.getPlayer().getInventory().getItemInMainHand() != null) {
-            if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.COMPASS) {
-                long t = System.currentTimeMillis();
-                long end = t+5000;
-                while (System.currentTimeMillis() < end) {
-
-                }
+            while (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.COMPASS) {
+                ItemStack compass = new ItemStack(Material.COMPASS);
+                CompassMeta compassMeta = (CompassMeta) compass.getItemMeta();
+                compassMeta.setLodestoneTracked(false);
+                compassMeta.setLodestone(runner.getLocation());
+                compassMeta.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Tracking Compass");
+                compass.setItemMeta(compassMeta);
+                event.getPlayer().getInventory().setItemInMainHand(compass);
             }
         }
     }
 }
-*/
-}
+
